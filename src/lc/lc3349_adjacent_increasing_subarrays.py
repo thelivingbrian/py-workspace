f5c6f1d312ba47151723a0f7d8a9b549110a81be
thelivingbrian <13973021+thelivingbrian@users.noreply.github.com>
2025-10-14 22:33:12 -0400

    add binary search solution

--- src/lc/lc3349_adjacent_increasing_subarrays.py
+++ src/lc/lc3349_adjacent_increasing_subarrays.py
@@ -61,3 +61,39 @@ class Solution:
             prev = num
         
         return max( out, streak//2, min(streak, prev_streak))
+
+# Binary search solution (Why?) nlogn time, n space - confusing
+class Solution:
+    def check(self, v, k):
+        for i in range(len(v) - k):
+            # Check for a value in dp that is at least k, and the value k indices away is also at least k
+            # e.g. k=3, arr = [1,2,3,1,2,3] -> dp = [1,2,3,1,2,3] => True: i[2] & i[2+k] >= k
+            if v[i] >= k and v[i + k] >= k:
+                return True
+        return False
+
+    def maxIncreasingSubarrays(self, nums):
+        n = len(nums)
+        if n == 2:
+            return 1
+
+        dp = [1] * n
+        for i in range(1, n):
+            # Maximum length of increasing subarray ending at index i
+            if nums[i] <= nums[i - 1]:
+                dp[i] = 1
+            else:
+                dp[i] = dp[i - 1] + 1
+
+        ans = 1
+        low, high = 1, n // 2 # k must be at least 1, at most n//2
+
+        # Binary search the range of valid k values
+        while low <= high:
+            mid = low + (high - low) // 2
+            if self.check(dp, mid):
+                ans = mid # Passes sliding window check, try for a larger k
+                low = mid + 1
+            else:
+                high = mid - 1
+        return ans
