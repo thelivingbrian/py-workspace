45578e39f2a9d1fde6019288b0c3ef02810beaec
thelivingbrian <13973021+thelivingbrian@users.noreply.github.com>
2025-11-01 22:33:46 -0400

    notes on rainwater redo

--- src/lc/lc407_trapping_rain_water_ii.py
+++ src/lc/lc407_trapping_rain_water_ii.py
@@ -44,10 +44,50 @@ class Solution:
                 seen[y+dy][x+dx] = True
                 if heightMap[y+dy][x+dx] < h:
                     total_water += h - heightMap[y+dy][x+dx]
-                    heightMap[y+dy][x+dx] = h
+                    heightMap[y+dy][x+dx] = h # This is the value that will get pushed
                 heapq.heappush(
                         boundary, 
                         (heightMap[y+dy][x+dx], y+dy, x+dx)
                     )
         
         return total_water
+    
+# redo
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        L, W = len(heightMap), len(heightMap[0])
+        if L<=2 or W <= 2: 
+            return 0
+        seen = [[False for _ in row] for row in heightMap]
+        heap = []
+        heapq.heapify(heap)
+
+        for i in range(L):
+            seen[i][0] = True
+            heapq.heappush(heap, (heightMap[i][0], i, 0))
+            seen[i][-1] = True
+            heapq.heappush(heap, (heightMap[i][-1], i, W-1))
+
+        for i in range(1, W-1):
+            seen[0][i] = True
+            heapq.heappush(heap, (heightMap[0][i], 0, i))
+            seen[-1][i] = True
+            heapq.heappush(heap, (heightMap[-1][i], L-1, i))
+
+        total = 0
+        while heap:
+            height, y, x = heapq.heappop(heap)
+            for y2, x2 in [(y+1,x), (y-1, x),(y, x+1), (y, x-1)]:
+                if y2 < 0 or y2 >= L or x2 < 0 or x2 >= W:
+                    continue
+                if seen[y2][x2]:
+                    continue
+                seen[y2][x2] = True
+                current = heightMap[y2][x2]
+                if current < height:
+                    total += height - current
+                    heapq.heappush(heap, (height, y2, x2))  # height is water level
+                else:
+                    heapq.heappush(heap, (current, y2, x2)) # current >= height
+
+        return total
